07840bff46647252bb8dcd1206bc4149021c86de
npp571 <npp57@njit.edu>
2024-11-15 11:34:20 -0500

    finished lab

--- lab.js
+++ lab.js
@@ -1,73 +1,103 @@
+//Question 1//
 let username = "Nistha";
 let age = 18;
-console.log(age);
+console.log(username, age);
 
 
-let rollnumber = 15;
-console.log(rollnumber);
+//Question 2//
 
-if (rollnumber > 10)
-    script = "RollNumber is greater than 10";
-    console.log(script);
+//string//
+let sport = "soccer";
+console.log(sport)
 
+//number//
+let no = 23
+console.log(no)
 
-
-let isMember;
-if (member = true){
-    isMember = "Member discount applied";
-    console.log(isMember)
+//boolean//
+let house;
+let people = 3;
+if(people >= 7){
+    house = "big house"
+    console.log(house)
+}
+else{
+    house = "small house"
+    console.log(house)
 }
 
+//undefined//
+let grade; 
+console.log(grade)
 
 
+//Question 3//
+let RollNumber = 15;
+if(RollNumber > 10){console.log("RollNumber is greater than 10")}
+
+
+//Question 4//
+let isMember = true;
+if (member = true){console.log("Member discount applied")}
+
+
+//Question 5//
 let startValue = 50;
 if (startValue > 0){
-    message = "Start value is a positive integer";
-    console.log(message);
+    console.log("Start value is a positive integer");
 }
-
 else if(startValue < 0){
-    message = "Start value is a negitive interger";
-    console.log(message);
+    console.log("Start value is a negitive interger");
 }
-
-else if(startVale = 0){
-    message = "Start valye is equal to 0";
-    console.log9(message);
+else if(startValue = 0){
+    console.log("Start value is equal to 0");
 }
 
 
-let a = 12
-let b = 234
-console.log(a > b,)
+//Question 6//
+let a = 12;
+let b = 234;
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
 
 
+//Question 7//
 let firstname = "Nistha";
 let lastname = "Patel";
 let ages = 18;
+console.log("Hello,", firstname, lastname, ",you are", ages, "years old.")
 
 
+//Question 8//
 let num = 50;
-
-if (num > 0, num < 100){
-    q8 = "The number is within range"
-    console.log(q8)
+if (num > 0 && num < 100){
+    console.log("The number is within range")
 }
+else {console.log("The number is not within range")}
 
+
+//Question 9//
 let hasCar = true;
 let hasLicence = false;
 let hasInsurance = true;
-
 if (hasCar && hasLicence && hasInsurance){
-    console.log("You can drive")
+    console.log("You can drive legally")
 }
 else {console.log("Check your driving eligibility")}
 
 
-
+//Question 10//
 let number = 21; 
+if(number % 2==0){console.log("The number is even")}
+else{console.log("The number is odd")}
 
 
+//Question 11//
 let score = 23;
 if (score >= 90){
     console.log("Grade A")
