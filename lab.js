f47fe9fcc8ab6cf2c6bb450485826047a3837f83
npp571 <npp57@njit.edu>
2024-11-15 09:27:30 -0500

    questions

---
+++ lab.js
@@ -0,0 +1,81 @@
+let username = "Nistha";
+let age = 18;
+console.log(age);
+
+
+let rollnumber = 15;
+console.log(rollnumber);
+
+if (rollnumber > 10)
+    script = "RollNumber is greater than 10";
+    console.log(script);
+
+
+
+let isMember;
+if (member = true){
+    isMember = "Member discount applied";
+    console.log(isMember)
+}
+
+
+
+let startValue = 50;
+if (startValue > 0){
+    message = "Start value is a positive integer";
+    console.log(message);
+}
+
+else if(startValue < 0){
+    message = "Start value is a negitive interger";
+    console.log(message);
+}
+
+else if(startVale = 0){
+    message = "Start valye is equal to 0";
+    console.log9(message);
+}
+
+
+let a = 12
+let b = 234
+console.log(a > b,)
+
+
+let firstname = "Nistha";
+let lastname = "Patel";
+let ages = 18;
+
+
+let num = 50;
+
+if (num > 0, num < 100){
+    q8 = "The number is within range"
+    console.log(q8)
+}
+
+let hasCar = true;
+let hasLicence = false;
+let hasInsurance = true;
+
+if (hasCar && hasLicence && hasInsurance){
+    console.log("You can drive")
+}
+else {console.log("Check your driving eligibility")}
+
+
+
+let number = 21; 
+
+
+let score = 23;
+if (score >= 90){
+    console.log("Grade A")
+}
+else if(score >80 <89){
+    console.log("Grade B")
+}
+else if(score < 80){console.log("Grade C")}
+
+
+
